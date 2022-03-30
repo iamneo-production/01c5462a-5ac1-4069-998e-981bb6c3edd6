170558ce179e0da1e8a461a757a2f9c682690d03
HarshithaVasudevan <harshiv2001@gmail.com>
2022-03-30 19:03:29 +0530

    login and signup - changes

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,12 +1,12 @@
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./Login.jsx";
-import Signup from "./Signup.jsx";
+import Login from "./components/Login/Login.jsx";
+import Signup from "./components/Signup/Signup.jsx";
 import { AuthProvider } from "./auth.js";
 //import { RequireAuthentication } from "./requireauthentication.js";
 
-import Home from "./Home.jsx";
+import Home from "./components/Home/Home.jsx";
 import Error from "./Error";
 function App() {
   return (
